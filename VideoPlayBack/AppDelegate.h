22a7da013374d96fd379916336d127731a320d60
1174062992 <1174062992>
2018-02-07 14:36:14 +0800

    Initial Commit

---
+++ VideoPlayBack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoPlayBack
+//
+//  Created by 曹记 on 2018/2/7.
+//  Copyright © 2018年 曹记. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
